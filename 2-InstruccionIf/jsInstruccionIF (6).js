cdfcd28370fb74e39464d70aa46a5a87085b9d13
alumno <alumno>
2017-02-07 12:19:09 -0300

    If, If anidado, || (o), && (y),

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,17 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad 
+    var edad= document.getElementById("edad").value;
+    if(edad>=18)
+    {
+        alert("Mayor de edad.");
+    }else{
+        if(edad>=13 && edad<=17){
+            alert("Adolescente.");
+        }else{
+            alert("Menor de edad.");
+        }
+    }
 
-
-
-
+    
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
 //tomo la edad  
-
+    var edad= document.getElementById("edad").value;
+    var ec= document.getElementById("estadoCivil").value="Casado";
+    if(edad<18 && ec!="Casado"){
+        alert("Es muy pequeño para estar casado/divorciado.");
+    
+    }
 	
 
 
